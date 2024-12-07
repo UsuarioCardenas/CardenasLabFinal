1fd23de4f3cae33dad79ce4d140c5cc86d1d199a
UsuarioCardenas <diego.cardenas.g@tecsup.edu.pe>
2024-12-06 20:44:25 -0500

    Primer commit del proyecto Android Studio

---
+++ app/src/main/java/com/cardenas/diego/lab13/GalleryActivity.kt
@@ -0,0 +1,22 @@
+package com.cardenas.diego.lab13
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cardenas.diego.lab13.databinding.ActivityGalleryBinding
+import java.io.File
+
+class GalleryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val directory = File(externalMediaDirs[0].absolutePath)
+        val files = directory.listFiles() as Array<File>
+        val adapter = GalleryAdapter(files.reversedArray())
+        binding.viewPager.adapter=adapter
+    }
+}
